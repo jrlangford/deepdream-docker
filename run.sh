57caf14e3c0c2d2be2d0c47169575c52a80b6066
Jonathan Robin Langford <jonathan.langford@schibsted.com.mx>
2015-07-20 17:10:54 -0500

    Download Base DNN model as part of the setup process

--- run.sh
+++ run.sh
@@ -1,3 +1,10 @@
 #!/bin/bash
 
+BASE_MODEL=$(pwd)/workspace/models/bvlc_googlenet/bvlc_googlenet.caffemodel
+if [ ! -f $BASE_MODEL ] 
+then
+	echo "Downloading base DNN model"
+	wget -O $BASE_MODEL http://dl.caffe.berkeleyvision.org/bvlc_googlenet.caffemodel
+fi
+
 docker run -d -v $(pwd)/workspace:/root/data --name=deepdream -p 8888:8888 jrlangford/deepdream
